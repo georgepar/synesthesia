d11f91ab89a95c2a0af9822602e39513500485d0
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2017-08-11 12:02:31 +0300

    Create data module with the Dataset class
    Data will be wrapped in the Dataset class. This class is a variation of the one
    in
    https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/learn/python/learn/datasets/mnist.py
    
    and provides methods for shuffling, batching and organizing the data and their
    labels.
    
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

---
+++ corrnet/data/dataset.py
@@ -0,0 +1,115 @@
+from __future__ import absolute_import
+from __future__ import division
+
+import collections
+import numpy as np
+from six.moves import xrange  # pylint: disable=redefined-builtin
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import random_seed
+
+
+Datasets = collections.namedtuple('Datasets', ['train', 'validation', 'test'])
+
+
+class DataSet(object):
+
+    def __init__(self,
+                 images,
+                 labels,
+                 fake_data=False,
+                 one_hot=False,
+                 dtype=dtypes.float32,
+                 seed=None):
+        """Construct a DataSet.
+        one_hot arg is used only if fake_data is true.  `dtype` can be either
+        `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
+        `[0, 1]`.  Seed arg provides for convenient deterministic testing.
+        """
+        seed1, seed2 = random_seed.get_seed(seed)
+        # If op level seed is not set, use whatever graph level seed
+        # is returned
+        np.random.seed(seed1 if seed is None else seed2)
+        dtype = dtypes.as_dtype(dtype).base_dtype
+        if dtype not in (dtypes.uint8, dtypes.float32):
+            raise TypeError('Invalid image dtype {}, expected uint8 or float32'
+                            .format(dtype))
+        if fake_data:
+            self._num_examples = 10000
+            self.one_hot = one_hot
+        else:
+            assert images.shape[0] == labels.shape[0], (
+                'images.shape: {} labels.shape: {}'
+                .format(images.shape, labels.shape))
+            self._num_examples = images.shape[0]
+
+            if dtype == dtypes.float32:
+                # Convert from [0, 255] -> [0.0, 1.0].
+                images = images.astype(np.float32)
+                images = np.multiply(images, 1.0 / 255.0)
+        self._images = images
+        self._labels = labels
+        self._epochs_completed = 0
+        self._index_in_epoch = 0
+
+    @property
+    def images(self):
+        return self._images
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def num_examples(self):
+        return self._num_examples
+
+    @property
+    def epochs_completed(self):
+        return self._epochs_completed
+
+    def next_batch(self, batch_size, fake_data=False, shuffle=False):
+        """Return the next `batch_size` examples from this data set."""
+        if fake_data:
+            fake_image = [1] * 784
+            if self.one_hot:
+                fake_label = [1] + [0] * 9
+            else:
+                fake_label = 0
+            return ([fake_image for _ in xrange(batch_size)],
+                    [fake_label for _ in xrange(batch_size)])
+        start = self._index_in_epoch
+        # Shuffle for the first epoch
+        if self._epochs_completed == 0 and start == 0 and shuffle:
+            perm0 = np.arange(self._num_examples)
+            np.random.shuffle(perm0)
+            self._images = self.images[perm0]
+            self._labels = self.labels[perm0]
+        # Go to the next epoch
+        if start + batch_size > self._num_examples:
+            # Finished epoch
+            self._epochs_completed += 1
+            # Get the rest examples in this epoch
+            rest_num_examples = self._num_examples - start
+            images_rest_part = self._images[start:self._num_examples]
+            labels_rest_part = self._labels[start:self._num_examples]
+            # Shuffle the data
+            if shuffle:
+                perm = np.arange(self._num_examples)
+                np.random.shuffle(perm)
+                self._images = self.images[perm]
+                self._labels = self.labels[perm]
+            # Start next epoch
+            start = 0
+            self._index_in_epoch = batch_size - rest_num_examples
+            end = self._index_in_epoch
+            images_new_part = self._images[start:end]
+            labels_new_part = self._labels[start:end]
+            concat_imgs = np.concatenate(
+                (images_rest_part, images_new_part), axis=0)
+            concat_labels = np.concatenate(
+                (labels_rest_part, labels_new_part), axis=0)
+            return concat_imgs, concat_labels
+        else:
+            self._index_in_epoch += batch_size
+            end = self._index_in_epoch
+            return self._images[start:end], self._labels[start:end]
