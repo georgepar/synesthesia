a8a94cbb1de0d535c9a3b8a8369ac92ea54b4b82
Giorgos Paraskevopoulos <geopar@central.ntua.gr>
2017-08-11 12:15:30 +0300

    Add general utility functions
    These functions can be used by every module
    
    Signed-off-by: Giorgos Paraskevopoulos <geopar@central.ntua.gr>

---
+++ corrnet/utils.py
@@ -0,0 +1,27 @@
+from __future__ import division
+import tensorflow as tf
+import toolz
+import corrnet.config as corrnet_cfg
+
+
+def get_num_iterations(num_samples):
+    return int(num_samples / corrnet_cfg.train.batch_size)
+
+
+def square_sum(x):
+    return tf.reduce_sum(tf.square(x))
+
+
+def l2_error(x1, x2):
+    return square_sum(tf.subtract(x1, x2))
+
+
+def compose_layers(layers):
+    # Compose apply functions for the list of layers
+    # reversing the layer list to apply in the correct order
+    if len(layers) == 0:
+        return lambda x: x
+    elif len(layers) == 1:
+        return layers[0]
+    else:
+        return toolz.functoolz.compose(*reversed([l for l in layers]))
