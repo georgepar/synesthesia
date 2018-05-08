602a4d5666e63eaa1a517af9260c10ae7526a095
Manos Zaranis <manoszar96@gmail.com>
2018-05-08 22:55:19 +0300

    text_parser change tabs to spaces

--- crossmodal/parsers/text/text_parser.py
+++ crossmodal/parsers/text/text_parser.py
@@ -9,23 +9,28 @@ class Text_Parser:
         if os.path.exists(Dataset_path):
             self.datasetPath = Dataset_path
         else:
-            assert os.path.lexists(Dataset_path),'Error: dataset path does not exists!'
+            assert os.path.lexists(Dataset_path), 'Error:  dataset ' \
+                                                 'path does not exists!'
 
     """
-    This method returns a dictionary containing the text_data of the dataset
+    This method returns a dictionary containing the text_data of the 
+    dataset
     Dictionary keys: Utterance-id,Gender,Transcription
     """
     def utterance_parser(self):
 
-        text_attr_dict = {"Utterance-Id":[],"Gender":[],"Transcription":[] }
+        text_attr_dict = {"Utterance-Id":[],"Gender":[],
+                          "Transcription":[] }
         Utterance_id_list = []
         Gender_list = []
         Transcription_list= []
-        listofSessions = ["Session1","Session2","Session3","Session4","Session5"]
+        listofSessions = ["Session1", "Session2", "Session3",
+                          "Session4", "Session5"]
 
         for Session in listofSessions:
 
-            path = self.datasetPath+"/"+Session+"/dialog/transcriptions/"
+            path = self.datasetPath+"/"+Session+"/dialog/" \
+                                                "transcriptions/"
             contained_files = glob.glob(path+"*.txt")
 
             for utt_file in contained_files:
@@ -33,15 +38,24 @@ class Text_Parser:
                     for line in inputfile.readlines():
                         (utterance_key,transcription) = line.split(":")
                         utterance_key = line.split(" ")[0]
-                        #utterance_key must start with Ses and not contains MX.. or FX..
-                        if( utterance_key.startswith("Ses") and ( "MX" not in utterance_key) and ( "FX" not in utterance_key) ):
-                            #delete newline on transcription end and also delete space at transcription's start
-                            transcription = transcription[1:len(transcription)-1]
-                            gender = utterance_key[len(utterance_key)-4:len(utterance_key)-3]
+                        #utterance_key must start with Ses and  not
+                        # contains MX.. or FX..
+                        if(utterance_key.startswith("Ses")
+                                and ("MX" not in utterance_key)
+                                and ("FX" not in utterance_key)):
+                            #delete newline on transcription end  and
+                            #also delete space at transcription's
+                            #start
+                            transcription =  transcription[1:len(
+                                transcription)-1]
+                            gender =  utterance_key[len(
+                                utterance_key)-4:len(utterance_key)-3]
                             #add all to dict
-                            text_attr_dict['Utterance-Id'].append(utterance_key)
+                            text_attr_dict['Utterance-Id'].append(
+                                utterance_key)
                             text_attr_dict['Gender'].append(gender)
-                            text_attr_dict['Transcription'].append(transcription)
+                            text_attr_dict['Transcription'].append(
+                                transcription)
 
         return text_attr_dict
 
