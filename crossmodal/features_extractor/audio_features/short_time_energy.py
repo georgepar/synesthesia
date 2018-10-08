c9d02887159333a3cde6f96dff53807a27a060bb
manzar96 <manoszar96@gmail.com>
2018-10-08 21:50:43 +0300

    Frame Size and Hop for Overlap iven in samples

--- crossmodal/features_extractor/audio_features/short_time_energy.py
+++ crossmodal/features_extractor/audio_features/short_time_energy.py
@@ -4,25 +4,26 @@ import numpy as np
 """
 This function returns the short_time_energy of a wav file.
 The function takes as input the normalized wav file(in numpy array
-form)  the relative freq, the desirable window_size(in ms) and the
-desirable overlap for frame segmentation.
-The window_size is by default 20ms.
-The overlap is by default 0.5 (50ms)
+form)  the relative freq, the desirable window_size(frame size) and the
+desirable overlap for frame segmentation (in samples).
+The window_size is by default 2048 samples.
+The hop (used for overlapping) is by default 512 samples. 
 The returned value is a short time energy numpy array (each element 
 is  the short time energy for each  frame)
 """
 
 
-def short_time_energy(wav, freq, window_size=20, overlap=0.5):
+def short_time_energy(wav, window_size=2048, hop=512):
 
-    # calculate frame's length in samples
-    frame_length = round(freq * window_size*(10**-3))
-    # calculate hop for overlap in samples
-    hop_length = round((1-overlap)*frame_length)
+    # frame's length in samples
+    frame_length = window_size
+
+    hop_length = hop
 
     rmse_vector = rmse(y=wav, frame_length=frame_length,
                        hop_length=hop_length)
 
+    #calculate short time energy from rmse
     short_time_energy_vector = np.power(rmse_vector, 2) * frame_length
 
     return short_time_energy_vector
