7d7a6c9001fc8ee929edeaf4022981ec168d39cf
manzar96 <manoszar96@gmail.com>
2018-10-08 21:55:19 +0300

    frame size and hop calc in samples

--- crossmodal/features_extractor/audio_features/zcr.py
+++ crossmodal/features_extractor/audio_features/zcr.py
@@ -5,21 +5,21 @@ import numpy as np
 """
 This function returns the zcr of a wav file.
 The function takes as input the normalized wav file(in numpy array 
-form)  the relative freq, the desirable window_size(in ms) and the  
-desirable overlap for frame segmentation.
-The window_size is by default 20ms.
-The overlap is by default 0.5 (50ms)
+form)  the relative freq, the desirable window_size(in samples) and the  
+desirable overlap for frame segmentation in samples too.
+The window_size is by default 2048 samples.
+The hop (used for overlap) is by default 512 samples.
 The returned value is a Zcr numpy array (each element is the zcr for 
 each  frame)
 """
 
 
-def zcr(wav,freq,window_size=20,overlap=0.5):
+def zcr(wav, window_size=2048, hop=512):
 
-    # calculate frame's length in samples
-    frame_length =round(freq * window_size*(10**-3))
-    # calculate hop for overlap in samples
-    hop_length = round((1-overlap)*frame_length)
+    # frame's length in samples
+    frame_length =window_size
+    # hop for overlap in samples
+    hop_length = hop
 
     zcr_array = zero_crossing_rate(wav, frame_length=frame_length,
                                    hop_length=hop_length)
