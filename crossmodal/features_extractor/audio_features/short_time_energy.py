b6e0b7264911486588683d6b0fe39979c62e65fe
manzar96 <manoszar96@gmail.com>
2018-10-30 03:14:37 +0200

    changes in overlap

--- crossmodal/features_extractor/audio_features/short_time_energy.py
+++ crossmodal/features_extractor/audio_features/short_time_energy.py
@@ -19,13 +19,13 @@ Return value: the mean of the short time energies (extracted from each frame).
 """
 
 
-def short_time_energy(segment, window_size=2048, overlap=20, freq=16000):
+def short_time_energy(segment, window_size=2048, overlap=0, freq=16000):
 
     # frame's length in samples
     frame_length = window_size
 
     # calculate hop length
-    samples = freq * overlap *1e-3
+    samples = freq * overlap * 10e-3
 
     assert samples < window_size,"The given overlap is more than frame's size"
 
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     audio_array = (np.asarray(wav.get_array_of_samples())).astype(np.float64)
     audio_freq = wav.frame_rate
     print(audio_array.shape)
-    a = short_time_energy(audio_array,window_size=2048,overlap=0)
+    a = short_time_energy(audio_array,window_size=2048)
     print(a.shape)
 
 
