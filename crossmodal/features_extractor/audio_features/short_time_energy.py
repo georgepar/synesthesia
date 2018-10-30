ab3f7ba42ceda2ef6c66278b3f2ea0c2951a2539
manzar96 <manoszar96@gmail.com>
2018-10-30 02:39:26 +0200

    changes in short time energy function

--- crossmodal/features_extractor/audio_features/short_time_energy.py
+++ crossmodal/features_extractor/audio_features/short_time_energy.py
@@ -2,19 +2,38 @@ from librosa.feature import rmse
 import numpy as np
 
 """
-This function returns the short time energy of a given segment. The sigment
+This function returns the short time energy of a given segment. The segment
 must be in numpy array form.
+
+This function takes as inputs:
+
+segment: a numpy array (float) representing the current segment from which we
+will receive the short time energy.
+
+window_size: librosa.rmse cuts into frames the given segment, so the 
+window_size represents the length of each frame.
+
+overlap: the overlap that the frames will with each other. It is given in msec.
+
+Return value: the mean of the short time energies (extracted from each frame).
 """
 
 
-def short_time_energy(wav, window_size=2048, hop=512):
+def short_time_energy(segment, window_size=2048, overlap=20, freq=16000):
 
     # frame's length in samples
     frame_length = window_size
 
-    hop_length = hop
+    #calculate hop length
+    samples = freq * overlap *1e-3
 
-    rmse_vector = rmse(y=wav, frame_length=frame_length,
+    assert samples < window_size,"The given overlap is more than frame's size"
+
+
+    hop_length = frame_length - samples
+
+    #calculat rmse
+    rmse_vector = rmse(y=segment, frame_length=frame_length,
                        hop_length=hop_length)
 
     #calculate short time energy from rmse
@@ -22,3 +41,11 @@ def short_time_energy(wav, window_size=2048, hop=512):
 
     return short_time_energy_vector
 
+
+if __name__ == "__main__":
+
+
+    path = '/home/manzar/Desktop/IEMOCAP/Session1/sentences/wav/Ses01M_impro01/Ses01M_impro01_M000.wav'
+    
+    b = short_time_energy(a,2,overlap=0)
+    print(b)
